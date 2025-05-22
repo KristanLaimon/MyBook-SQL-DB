b95b9522ae6e0715325c55c2c1a97601256cef23
Jesus Eduardo Alvarez Perez <92644723+DASTLSITO@users.noreply.github.com>
2025-05-21 18:18:51 -0700

    Fixing sum

--- Final Proyect/FinalProyect.test.sql
+++ Final Proyect/FinalProyect.test.sql
@@ -15,7 +15,7 @@ select distinct ID from Books order by ID;
 select distinct ID from Users;
 
 -- TO CHECK ALL INDEXES FROM A TABLE
-go;
+go
 select *
 from sys.indexes
 where object_id = object_id('Book_User_Reads');
